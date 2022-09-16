60483a221d03d1ea9e5455d758949c6d0635730a
Mohibullah Baig <mohibullah.baig727@gmail.com>
2022-09-16 20:41:24 +0200

    delete saved shows

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -3,6 +3,7 @@ import { UserAuth } from "../context/AuthContext";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { db } from "../firebase";
 import { updateDoc, doc, onSnapshot } from "firebase/firestore";
+import { AiOutlineClose } from "react-icons/ai";
 
 const SavedShows = () => {
   const [movies, setMovies] = useState([]);
@@ -16,6 +17,18 @@ const SavedShows = () => {
   const slideRight = () => {
     var slider = document.getElementById("slider");
     slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
+  const movieRef = doc(db, "users", `${user.email}`);
+  const deleteShow = async (passedId) => {
+    try {
+      const result = movies.filter((item) => item.id !== passedId);
+      await updateDoc(movieRef, {
+        savedShow: result,
+      });
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   useEffect(() => {
@@ -47,6 +60,12 @@ const SavedShows = () => {
               <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white ">
                 <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
                   {item?.title}
+                </p>
+                <p
+                  onClick={() => deleteShow(item.id)}
+                  className="absolute text-gray-300 top-4 right-4"
+                >
+                  <AiOutlineClose />
                 </p>
               </div>
             </div>
