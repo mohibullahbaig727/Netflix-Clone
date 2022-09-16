523240df5376c1c81b64bc31c4b15199bfe23ac8
Mohibullah Baig <mohibullah.baig727@gmail.com>
2022-09-16 20:13:41 +0200

    account section liked movies

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -19,11 +19,9 @@ const SavedShows = () => {
   };
 
   useEffect(() => {
-    onSnapshot(
-      doc(db, "users", `${user?.email}`, (doc) => {
-        setMovies(doc.data()?.savedShow);
-      })
-    );
+    onSnapshot(doc(db, "users", `${user?.email}`), (doc) => {
+      setMovies(doc.data()?.savedShow);
+    });
   }, [user?.email]);
 
   return (
@@ -39,12 +37,12 @@ const SavedShows = () => {
           id={"slider"}
           className="w-full h-full left-0 overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
         >
-          {movies.map((item, id) => (
+          {movies?.map((item, id) => (
             <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
               <img
                 className="w-full h-auto block"
                 src={`https://image.tmdb.org/t/p/w500/${item?.img}`}
-                alt={item.title}
+                alt={item?.title}
               />
               <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white ">
                 <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
--- src/components/pages/Account.jsx
+++ src/components/pages/Account.jsx
@@ -10,12 +10,12 @@ const Account = () => {
           src="https://assets.nflxext.com/ffe/siteui/vlv3/701eec50-4b87-4dc0-9d00-b0f54025dc36/d1bdc8ba-c91e-41a8-87a7-a871da5e699c/SE-en-20220905-popsignuptwoweeks-perspective_alpha_website_small.jpg"
           alt="/"
         />
-        <div className="bg-black/60 top-0 left-0 w-full h-[550px]"></div>
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
         <div className="absolute top-[20%] p-4 md:p-8">
           <h1 className="text-3xl md:text-5xl font-bold"> My Shows</h1>
         </div>
-        <SavedShows />
       </div>
+      <SavedShows />
     </>
   );
 };
